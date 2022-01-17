c450372503d9d8f0a729aa657fffbdad1b0f4fbc
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2022-01-17 13:36:39 +0200

    Добавляет реализацию виджета статистики
    Без стилизации

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Statistics from "./components/Statistics";
+// import Counter from "./components/Counter";
+
+const App = () => {
+  return (
+    <>
+      <h1>Homework 3</h1>
+      {/* <Counter initialValue={0} /> */}
+      <Statistics />
+    </>
+  );
+}
+
+export default App;
